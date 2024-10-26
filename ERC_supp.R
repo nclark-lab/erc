1d977ed2800a214b13fefeac39fdb6ae7d8f0777
Guillermo Hoffmann <84525324+glhoffmann@users.noreply.github.com>
2024-10-26 09:45:29 -0500

    Add files via upload

---
+++ ERC_supp.R
@@ -0,0 +1,69 @@
+##supplementary ERC functions
+##updated JHL 10/10/24
+
+
+##make sure the gene list is contained in the erc matrix
+clean_list = function(list, names){
+  list <- unique(list)
+  list <- list[which(match(list,names) != "NA")]
+  list
+}
+
+pair_list <- function(list, erc_matrix , na.val = -2) {
+  #provide vector list of genes (row and col names) and erc_matrix
+  #returns vector list of gene x gene ERC values with no repeats or 'self x self' entries
+  # good for computing averages and making histograms, etc...
+  list <- clean_list(list , colnames(erc_matrix))
+  mat <- erc_matrix[list,list]
+  mat[which(mat == na.val)] <- NA
+  mat[lower.tri(mat)]
+}
+##Make the ERC matrix symmetrical
+make_symmetric = function(erc_matrix){
+  for(gene in rownames(erc_matrix)){
+    #print(gene)
+    to_fix = which(is.na(erc_matrix[,gene]))
+    temp = which(!is.na(erc_matrix[gene,to_fix]))
+    if(length(temp) ==0){
+      next
+    }
+    erc_matrix[names(temp),gene] = erc_matrix[gene,names(temp)]
+  }
+  erc_matrix
+}
+
+##get permutations for a list of genes against the genome
+permTestMat <- function(list, erc_matrix, perms=10000) {
+  cleannames <- clean_list(colnames(erc_matrix),colnames(erc_matrix))
+  list <- clean_list(list, cleannames)
+  if(length(list) == 1){return(NaN)}
+  obs <- mean(erc_matrix[list,list] , na.rm=TRUE)
+  print(obs)
+  if (is.nan(obs)) { return(NaN) }
+  
+  null <- c()
+  while (length(null) < perms) {
+    s <- sample(cleannames , length(list))
+    m <- mean(pair_list(s , erc_matrix) , na.rm=TRUE)
+    if (is.nan(m)) next
+    null <- c(null , m)
+  }
+  pval <- sum(as.numeric(obs <= null)) / perms
+  list( obs=obs , p=pval , null=null)
+}
+##Supply the output from computeERC to get the Fisher transformed correlation values
+fishertransformed_updated <- function(erc_matrix){
+  out <- erc_matrix$cor
+  out <- atanh(erc_matrix$cor)*sqrt((erc_matrix$count)-3)
+  ##out[upper.tri(out)] <- erc_matrix[upper.tri(erc_matrix)]
+  out[is.nan(out)] <- NA
+  out
+}
+
+##Get a subset matrix based on two gene lists
+betweencomplex <- function(list1 , list2 , erc_matrix) {
+  list1 <- clean_list(list1 , colnames(erc_matrix))
+  list2 <- clean_list(list2 , colnames(erc_matrix))
+  mat <- erc_matrix[list1,list2]
+  mat
+}
